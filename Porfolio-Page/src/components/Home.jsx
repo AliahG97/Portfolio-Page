2fde44a4b5f835aed7fe4943de04be3c1def45cd
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-04-05 15:51:43 -0400

    nothing works

---
+++ Porfolio-Page/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './components'; //
+function Home() {
+  return (
+    <div>
+      <Navigation />
+      {/*  content for the Home page */}
+    </div>
+  );
+}
+
+export default Home;
---
+++ Porfolio-Page/src/components/Resume.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div>
+      {/* Add your resume content here */}
+      <h2>Resume</h2>
+      <p>resume content.</p>
+    </div>
+  );
+}
+
+export default Resume;
