e20b0f884ba33c2b3e2cebe6029775fdfff88680
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-04-15 22:22:18 -0400

    navbar fixed

--- Porfolio-Page/src/components/Header.jsx
+++ Porfolio-Page/src/components/Header.jsx
@@ -3,36 +3,36 @@ import React from 'react';
 
 export default function Header(props) {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Aliah Guerra</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      
+        <a className="navbar-brand" href="#">Aliah Guerra</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Resume</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Resume</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Portfolio
               </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">YumYard</a></li>
-                <li><a class="dropdown-item" href="#">Music E-Shop</a></li>
-                <li><a class="dropdown-item" href="#">Music E-Shop</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="#">Other</a></li>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">YumYard</a></li>
+                <li><a className="dropdown-item" href="#">Music E-Shop</a></li>
+                <li><a className="dropdown-item" href="#">REACT</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Other</a></li>
               </ul>
             </li>
             {/* <li class="nav-item">
@@ -44,7 +44,6 @@ export default function Header(props) {
               <button class="btn btn-outline-success" type="submit">Search</button>
           </form> */}
         </div>
-      </div>
     </nav>
 
   );
